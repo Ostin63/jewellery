237611c4ee8e8385143e275356bd0a91244c017f
Dmitry <upssq@mail.ru>
2021-12-23 20:31:14 +0300

    Исправления таску в галп

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,13 @@ var svgstore = require('gulp-svgstore');
 var posthtml = require('gulp-posthtml');
 var include = require('posthtml-include');
 var del = require('del');
+var concat = require('gulp-concat');
+
+gulp.task('vendor-scripts', function () {
+  return gulp.src('source/js/vendor/*.js')
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('build/js'));
+});
 
 gulp.task('css', function () {
   return gulp.src('source/sass/style.scss')
@@ -87,7 +94,7 @@ gulp.task('copy', function () {
   return gulp.src([
     'source/fonts/**/*.{woff,woff2}',
     'source/img/**',
-    'source/js/**',
+    'source/js/main.js',
     'source//*.ico'
   ], {
     base: 'source'
@@ -108,5 +115,5 @@ gulp.task('clean', function () {
   return del('build');
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'css', 'sprite', 'html'));
+gulp.task('build', gulp.series('clean', 'copy', 'vendor-scripts', 'css', 'sprite', 'html'));
 gulp.task('start', gulp.series('build', 'server'));
